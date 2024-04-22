53ce086013d4bb08ee8672681fd41a35b9626f69
demchukv <demchukv@gmail.com>
2024-04-22 14:30:17 +0300

    complete

---
+++ index.js
@@ -0,0 +1,37 @@
+import { listContacts, getContactById, removeContact, addContact } from "./contacts.js";
+import { program } from "commander";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      return console.table(contacts);
+ 
+    case "get":
+      const contact = await getContactById(id);
+      return contact ? console.table(contact) : console.log("Contact not found");
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      return console.table(newContact);
+
+    case "remove":
+      const removed = await removeContact(id);
+      return removed ? console.log("Contact deleted: ", id) : console.log("Contact not found");
+
+    default:
+      return console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
